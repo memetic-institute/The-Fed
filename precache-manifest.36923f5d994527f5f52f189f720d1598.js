edcd90f78f35b8b54ddcc9ac784d629c027daf24
NPC64927130 <NPC64927130@memetic.institute>
2020-03-24 02:01:16 -0600

    Updates

---
+++ precache-manifest.36923f5d994527f5f52f189f720d1598.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df507e2b0c24d4e7aec9170aeecc548d",
+    "url": "https://thefed.app/index.html"
+  },
+  {
+    "revision": "e3b71b3f96803496553f",
+    "url": "https://thefed.app/static/css/1.66d48438.chunk.css"
+  },
+  {
+    "revision": "71892ceddb935e491ee1",
+    "url": "https://thefed.app/static/css/4.7ba9d008.chunk.css"
+  },
+  {
+    "revision": "4b25f3c3b9827f696b16",
+    "url": "https://thefed.app/static/css/6.612b79ea.chunk.css"
+  },
+  {
+    "revision": "72da117d21afe4f33e55",
+    "url": "https://thefed.app/static/css/7.02d74b59.chunk.css"
+  },
+  {
+    "revision": "01f3e0a42f42e234c8cb",
+    "url": "https://thefed.app/static/css/8.3b1a081c.chunk.css"
+  },
+  {
+    "revision": "587e58eca0fa6be051d0",
+    "url": "https://thefed.app/static/css/main.47039ba7.chunk.css"
+  },
+  {
+    "revision": "b5d9f2967dbce450f53a",
+    "url": "https://thefed.app/static/js/0.4c17d97b.chunk.js"
+  },
+  {
+    "revision": "573c03b54bf739ab0b9029188d0efb17",
+    "url": "https://thefed.app/static/js/0.4c17d97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b71b3f96803496553f",
+    "url": "https://thefed.app/static/js/1.f0604acb.chunk.js"
+  },
+  {
+    "revision": "71892ceddb935e491ee1",
+    "url": "https://thefed.app/static/js/4.5a1c4b6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://thefed.app/static/js/4.5a1c4b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf6c308e8092d8b222e",
+    "url": "https://thefed.app/static/js/5.69d3ede1.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "https://thefed.app/static/js/5.69d3ede1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b25f3c3b9827f696b16",
+    "url": "https://thefed.app/static/js/6.ff6a1ca2.chunk.js"
+  },
+  {
+    "revision": "72da117d21afe4f33e55",
+    "url": "https://thefed.app/static/js/7.1d285291.chunk.js"
+  },
+  {
+    "revision": "01f3e0a42f42e234c8cb",
+    "url": "https://thefed.app/static/js/8.9fc80a5b.chunk.js"
+  },
+  {
+    "revision": "587e58eca0fa6be051d0",
+    "url": "https://thefed.app/static/js/main.72e4d03f.chunk.js"
+  },
+  {
+    "revision": "855566296ee818328daa",
+    "url": "https://thefed.app/static/js/runtime-main.454087e5.js"
+  },
+  {
+    "revision": "f7d3bce2a2770ca4041e33e14e87c7ab",
+    "url": "https://thefed.app/static/media/apple.f7d3bce2.png"
+  },
+  {
+    "revision": "829c92a14951977039eb3e39ed6eaa06",
+    "url": "https://thefed.app/static/media/bank-note-pattern.829c92a1.svg"
+  },
+  {
+    "revision": "1cc65d61d2bc80bf23053b00728b345f",
+    "url": "https://thefed.app/static/media/black-op.1cc65d61.jpg"
+  },
+  {
+    "revision": "a7ce23f9323c57de38a6b9dadddaa5ab",
+    "url": "https://thefed.app/static/media/bribe.a7ce23f9.jpg"
+  },
+  {
+    "revision": "acb54da070f6445c9693c061e04a1337",
+    "url": "https://thefed.app/static/media/cia.acb54da0.svg"
+  },
+  {
+    "revision": "56c8ec01926cdb8f05a929a6538938bb",
+    "url": "https://thefed.app/static/media/cuba.56c8ec01.png"
+  },
+  {
+    "revision": "e7fe049ab2e36d13bac0f71abe0c0ea9",
+    "url": "https://thefed.app/static/media/espionage.e7fe049a.jpg"
+  },
+  {
+    "revision": "e280302d30f742e9202442a032c22907",
+    "url": "https://thefed.app/static/media/federal-reserve.e280302d.png"
+  },
+  {
+    "revision": "a35fbb122c9ed67800089f681df25030",
+    "url": "https://thefed.app/static/media/imrd.a35fbb12.svg"
+  },
+  {
+    "revision": "329c318e8a658935177486b69bc41d03",
+    "url": "https://thefed.app/static/media/insurance-fraud.329c318e.jpg"
+  },
+  {
+    "revision": "6ca69add9430602519611afc7b95e261",
+    "url": "https://thefed.app/static/media/jpm.6ca69add.png"
+  },
+  {
+    "revision": "936a6be3f4a40d9ae338362179a51ea6",
+    "url": "https://thefed.app/static/media/mint.936a6be3.jpg"
+  },
+  {
+    "revision": "e332e1735f90ebdc70d7f26d5d4d9a1d",
+    "url": "https://thefed.app/static/media/money-press.e332e173.jpg"
+  },
+  {
+    "revision": "4f0f3ec00bf5c0e484c170ddacd6e014",
+    "url": "https://thefed.app/static/media/north-korea.4f0f3ec0.png"
+  },
+  {
+    "revision": "40964e0e00babf8e2b824ec7769836bf",
+    "url": "https://thefed.app/static/media/oligarch.40964e0e.jpg"
+  },
+  {
+    "revision": "764420fe5dec5a61b8c63fd9ed4a87c6",
+    "url": "https://thefed.app/static/media/print.764420fe.mp3"
+  },
+  {
+    "revision": "20488d98327edd211ab88efbbdfefe4f",
+    "url": "https://thefed.app/static/media/propaganda.20488d98.jpg"
+  },
+  {
+    "revision": "b07e12ff81258aae9c9450f43fd1e913",
+    "url": "https://thefed.app/static/media/realDonaldTrump.b07e12ff.jpg"
+  },
+  {
+    "revision": "339903223f0bcc15169faea044da95cf",
+    "url": "https://thefed.app/static/media/rubber-stamp.33990322.jpg"
+  },
+  {
+    "revision": "5e1ab361f8cf6f3f4c1ff242f5b3230f",
+    "url": "https://thefed.app/static/media/till.5e1ab361.mp3"
+  },
+  {
+    "revision": "41aa06140cd9116a555631189631c925",
+    "url": "https://thefed.app/static/media/trump-signature.41aa0614.svg"
+  },
+  {
+    "revision": "f0988606930e863a8352f125c543fddd",
+    "url": "https://thefed.app/static/media/tweet.f0988606.mp3"
+  },
+  {
+    "revision": "4dc7de3347f48884a7fb27e0ba6b8f6b",
+    "url": "https://thefed.app/static/media/vt323-latin-400.4dc7de33.woff2"
+  },
+  {
+    "revision": "d037f8cb75b5764ae757659ae551cc62",
+    "url": "https://thefed.app/static/media/vt323-latin-400.d037f8cb.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://thefed.app/precache-manifest.36923f5d994527f5f52f189f720d1598.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://thefed.app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
